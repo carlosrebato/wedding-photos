64776edee9f738f64246bad0fda6a7728bc06d67
Carlos Rebato <carlosrebato.mail@gmail.com>
2025-12-02 19:15:56 +0100

    cambio de modal sin tarjeta a una sosa 3

--- app/page.tsx
+++ app/page.tsx
@@ -904,7 +904,7 @@ export default function Home() {
             <img
               src="/assets/arbolito.png"
               alt=""
-              className="w-20 h-20 object-contain"
+              className="w-30 h-30 object-contain"
             />
 
             <h2
